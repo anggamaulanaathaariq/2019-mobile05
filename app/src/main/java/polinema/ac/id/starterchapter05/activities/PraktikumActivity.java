048031fa3ea1c748ea7ac04c568c35d9cde7f732
Angga Maulana Athaariq <am.athaariq@gmail.com>
2019-09-28 18:05:46 +0700

    Praktikum Activity ( Update )

--- app/src/main/java/polinema/ac/id/starterchapter05/activities/PraktikumActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/PraktikumActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 
 import polinema.ac.id.starterchapter05.R;
 import polinema.ac.id.starterchapter05.fragments.PraktikumFragment;
+import polinema.ac.id.starterchapter05.fragments.RedFragment;
 
 public class PraktikumActivity extends AppCompatActivity {
 
@@ -19,9 +20,16 @@ public class PraktikumActivity extends AppCompatActivity {
 
     public void handleClickHandstandFragment(View view){
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.praktikum_fragment_placeholder,new PraktikumFragment(),"PRAKTIKUM_FRAGMENT");
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
+        if (getSupportFragmentManager().findFragmentByTag("PRAKTIKUM_FRAGMENT") != null) {
+            fragmentTransaction.setCustomAnimations(R.anim.enter_from_left, R.anim.exit_from_left, R.anim.enter_from_left, R.anim.exit_from_left);
+            fragmentTransaction.replace(R.id.praktikum_fragment_placeholder,new PraktikumFragment(),"PRAKTIKUM_FRAGMENT");
+            fragmentTransaction.addToBackStack(null);
+        } else {
+            fragmentTransaction.setCustomAnimations(R.anim.enter_from_left, R.anim.exit_from_left, R.anim.enter_from_left, R.anim.exit_from_left);
+            fragmentTransaction.replace(R.id.praktikum_fragment_placeholder, new PraktikumFragment(), "PRAKTIKUM_FRAGMENT");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
 
     }
 }
