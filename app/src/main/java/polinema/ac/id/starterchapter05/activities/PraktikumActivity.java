45c015962ce7dbc563afa48a650f7310366f3141
Angga Maulana Athaariq <am.athaariq@gmail.com>
2019-09-28 18:23:32 +0700

    Praktikum Activity ( Update )

--- app/src/main/java/polinema/ac/id/starterchapter05/activities/PraktikumActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/PraktikumActivity.java
@@ -22,11 +22,11 @@ public class PraktikumActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         if (getSupportFragmentManager().findFragmentByTag("PRAKTIKUM_FRAGMENT") != null) {
             fragmentTransaction.setCustomAnimations(R.anim.enter_from_left, R.anim.exit_from_left, R.anim.enter_from_left, R.anim.exit_from_left);
-            fragmentTransaction.replace(R.id.praktikum_fragment_placeholder,new PraktikumFragment(),"PRAKTIKUM_FRAGMENT");
+            fragmentTransaction.replace(R.id.fragmentHolder,new PraktikumFragment(),"PRAKTIKUM_FRAGMENT");
             fragmentTransaction.addToBackStack(null);
         } else {
             fragmentTransaction.setCustomAnimations(R.anim.enter_from_left, R.anim.exit_from_left, R.anim.enter_from_left, R.anim.exit_from_left);
-            fragmentTransaction.replace(R.id.praktikum_fragment_placeholder, new PraktikumFragment(), "PRAKTIKUM_FRAGMENT");
+            fragmentTransaction.replace(R.id.fragmentHolder, new PraktikumFragment(), "PRAKTIKUM_FRAGMENT");
             fragmentTransaction.addToBackStack(null);
             fragmentTransaction.commit();
         }
