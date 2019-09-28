b95965e0979fdeea1adbf9e911f8fb75964862ea
Angga Maulana Athaariq <am.athaariq@gmail.com>
2019-09-28 09:34:24 +0700

    Praktikum Activity

--- app/src/main/java/polinema/ac/id/starterchapter05/activities/PraktikumActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/PraktikumActivity.java
@@ -1,10 +1,13 @@
 package polinema.ac.id.starterchapter05.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 import polinema.ac.id.starterchapter05.R;
+import polinema.ac.id.starterchapter05.fragments.PraktikumFragment;
 
 public class PraktikumActivity extends AppCompatActivity {
 
@@ -12,5 +15,13 @@ public class PraktikumActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_praktikum);
+    }
+
+    public void handleClickHandstandFragment(View view){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.praktikum_fragment_placeholder,new PraktikumFragment(),"PRAKTIKUM_FRAGMENT");
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
     }
 }
